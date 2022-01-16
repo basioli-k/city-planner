fdba4c30de1d2a7f761db85177996908c428107e
basioli-k <k.basioli@gmail.com>
2022-01-16 21:47:22 +0100

    database for road and node

--- Node.cs
+++ Node.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,68 @@ namespace city_planner
 {
     class Node
     {
-        private int id;
-        private int x;
-        private int y;
+        private long id;
+        private long x;
+        private long y;
         private List<string> characteristics;
 
         Node() { }
+
+        Node(long id_, long x_, long y_, string characteristics_)
+        {
+            id = id_;
+            x = x_;
+            y = y_;
+            characteristics = characteristics_.Split(',').ToList<string>();
+        }
+        Node(long id_, long x_, long y_, List<string> characteristics_)
+        {
+            id = id_;
+            x = x_;
+            y = y_;
+            characteristics = characteristics_;
+        }
+        Node(long x_, long y_, string characteristics_)
+        {
+            x = x_;
+            y = y_;
+            characteristics = characteristics_.Split(',').ToList<string>();
+        }
+        Node(long x_, long y_, List<string> characteristics_)
+        {
+            x = x_;
+            y = y_;
+            characteristics = characteristics_;
+        }
+        public Node(SQLiteDataReader reader)
+        {
+            var temp = reader["id"];
+            id = (long)reader["id"];
+            x = (long)reader["x"];
+            y = (long)reader["y"];
+            characteristics = ((string)reader["characteristics"]).Split(',').ToList<string>();
+        }
+
+        public void insert()
+        {
+            string sql = "INSERT INTO Node (x, y, characteristics) VALUES(" +
+                x.ToString() + "," + y.ToString() + "," + String.Join(",", characteristics) + ");";
+
+            var db = Database.GetInstance();
+            db.ExecuteNonQuery(sql);
+        }
+
+        static public List<Node> select_star()
+        {
+            string sql = "SELECT * FROM Node;";
+            var db = Database.GetInstance();
+            return db.ExecuteQuery<Node>(sql);
+        }
+
+        public override string ToString()
+        {
+            return id.ToString() + "," + x.ToString() + "," + y.ToString() + "," + String.Join(" ", characteristics);
+        }
 
         // insert, konstruktori odgovarajuci
     }
--- Road.cs
+++ Road.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,65 @@ namespace city_planner
 {
     class Road
     {
-        private int id;
-        private int src;
-        private int dest;
+        private long id;
+        private long src;
+        private long dest;
         private List<string> characteristics;
+
+        Road() { }
+
+        Road(long id_, long src_, long dest_, string characteristics_)
+        {
+            id = id_;
+            src = src_;
+            dest = dest_;
+            characteristics = characteristics_.Split(',').ToList<string>();
+        }
+        Road(long id_, long src_, long dest_, List<string> characteristics_)
+        {
+            id = id_;
+            src = src_;
+            dest = dest_;
+            characteristics = characteristics_;
+        }
+        Road(long src_, long dest_, string characteristics_)
+        {
+            src = src_;
+            dest = dest_;
+            characteristics = characteristics_.Split(',').ToList<string>();
+        }
+        Road(long src_, long dest_, List<string> characteristics_)
+        {
+            src = src_;
+            dest = dest_;
+            characteristics = characteristics_;
+        }
+        public Road(SQLiteDataReader reader)
+        {
+            id = (long)reader["id"];
+            src = (long)reader["src"];
+            dest = (long)reader["dest"];
+            characteristics = ((string)reader["characteristics"]).Split(',').ToList<string>();
+        }
+
+        public void insert()
+        {
+            string sql = "INSERT INTO Node (src, dest, characteristics) VALUES(" +
+                src.ToString() + "," + dest.ToString() + "," + String.Join(",", characteristics) + ");";
+
+            var db = Database.GetInstance();
+            db.ExecuteNonQuery(sql);
+        }
+
+        static public List<Road> select_star()
+        {
+            string sql = "SELECT * FROM Road;";
+            var db = Database.GetInstance();
+            return db.ExecuteQuery<Road>(sql);
+        }
+        public override string ToString()
+        {
+            return id.ToString() + "," + src.ToString() + "," + dest.ToString() + "," + String.Join(" ", characteristics);
+        }
     }
 }
