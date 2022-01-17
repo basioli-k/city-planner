f552e2992063a5830399e0725ae08a24c35713a3
Duricant <antonio.duric77@gmail.com>
2022-01-17 16:09:23 +0100

    crtanje tocke

--- CityPlan.cs
+++ CityPlan.cs
@@ -19,15 +19,29 @@ namespace city_planner
             InitializeComponent();
         }
 
-        private void CityPlan_Click(object sender, EventArgs e)
+        private void CityPlan_MouseClick(object sender, MouseEventArgs e)
         {
-            var x = MousePosition.X - Location.X;
-            var y = MousePosition.Y - Location.Y;
+            var x = e.X;
+            var y = e.Y;
             var database = Database.GetInstance();
+
+            crtaj_tocku(x, y);
+
+
             // if checked dodaj_cvor/add_vertex ...
-                // u bazu dodaj cvor
-                // draw_plan(filter) 
+            // u bazu dodaj cvor
+            // draw_plan(filter) 
             // 
         }
+
+
+        void crtaj_tocku(int x, int y)
+        {
+            var g = CreateGraphics();
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+            g.DrawEllipse(Pens.Black, x - (int)vertex_radius / 2, y - (int)vertex_radius / 2, (float)vertex_radius, (float)vertex_radius);
+        }
+
     }
 }
