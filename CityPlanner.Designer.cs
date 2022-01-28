e0e13d0f46181741a134bf7c96d990712869fbcb
basioli-k <k.basioli@gmail.com>
2022-01-28 15:50:20 +0100

    icona

--- CityPlanner.Designer.cs
+++ CityPlanner.Designer.cs
@@ -54,10 +54,12 @@ namespace city_planner
             this.radioButton8 = new System.Windows.Forms.RadioButton();
             this.radioButton7 = new System.Windows.Forms.RadioButton();
             this.cityPlan1 = new city_planner.CityPlan();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -66,7 +68,7 @@ namespace city_planner
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
-            this.tabControl1.Location = new System.Drawing.Point(812, 122);
+            this.tabControl1.Location = new System.Drawing.Point(812, 158);
             this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -80,11 +82,11 @@ namespace city_planner
             this.tabPage1.Controls.Add(this.radioButton3);
             this.tabPage1.Controls.Add(this.radioButton2);
             this.tabPage1.Controls.Add(this.radioButton1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
             this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage1.Size = new System.Drawing.Size(459, 450);
+            this.tabPage1.Size = new System.Drawing.Size(459, 446);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Controls";
             // 
@@ -94,7 +96,7 @@ namespace city_planner
             this.radioButton3.Location = new System.Drawing.Point(9, 83);
             this.radioButton3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(112, 21);
+            this.radioButton3.Size = new System.Drawing.Size(133, 24);
             this.radioButton3.TabIndex = 2;
             this.radioButton3.Text = "Delete Object";
             this.radioButton3.UseVisualStyleBackColor = true;
@@ -106,7 +108,7 @@ namespace city_planner
             this.radioButton2.Location = new System.Drawing.Point(9, 44);
             this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(82, 21);
+            this.radioButton2.Size = new System.Drawing.Size(96, 24);
             this.radioButton2.TabIndex = 1;
             this.radioButton2.Text = "Add Line";
             this.radioButton2.UseVisualStyleBackColor = true;
@@ -118,7 +120,7 @@ namespace city_planner
             this.radioButton1.Location = new System.Drawing.Point(9, 8);
             this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(87, 21);
+            this.radioButton1.Size = new System.Drawing.Size(102, 24);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.Text = "Add Point";
             this.radioButton1.UseVisualStyleBackColor = true;
@@ -137,11 +139,11 @@ namespace city_planner
             this.tabPage2.Controls.Add(this.textBox2);
             this.tabPage2.Controls.Add(this.textBox1);
             this.tabPage2.Controls.Add(this.label1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
             this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage2.Size = new System.Drawing.Size(459, 450);
+            this.tabPage2.Size = new System.Drawing.Size(459, 446);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Route planning";
             // 
@@ -151,7 +153,7 @@ namespace city_planner
             this.radioButton6.Location = new System.Drawing.Point(288, 48);
             this.radioButton6.Margin = new System.Windows.Forms.Padding(4);
             this.radioButton6.Name = "radioButton6";
-            this.radioButton6.Size = new System.Drawing.Size(76, 21);
+            this.radioButton6.Size = new System.Drawing.Size(89, 24);
             this.radioButton6.TabIndex = 12;
             this.radioButton6.Text = "Walking";
             this.radioButton6.UseVisualStyleBackColor = true;
@@ -163,7 +165,7 @@ namespace city_planner
             this.radioButton5.Location = new System.Drawing.Point(210, 48);
             this.radioButton5.Margin = new System.Windows.Forms.Padding(4);
             this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(70, 21);
+            this.radioButton5.Size = new System.Drawing.Size(85, 24);
             this.radioButton5.TabIndex = 11;
             this.radioButton5.Text = "Bicycle";
             this.radioButton5.UseVisualStyleBackColor = true;
@@ -175,7 +177,7 @@ namespace city_planner
             this.label4.Location = new System.Drawing.Point(10, 50);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(131, 17);
+            this.label4.Size = new System.Drawing.Size(155, 20);
             this.label4.TabIndex = 10;
             this.label4.Text = "Means of transport:";
             // 
@@ -186,7 +188,7 @@ namespace city_planner
             this.radioButton4.Location = new System.Drawing.Point(154, 48);
             this.radioButton4.Margin = new System.Windows.Forms.Padding(4);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(48, 21);
+            this.radioButton4.Size = new System.Drawing.Size(57, 24);
             this.radioButton4.TabIndex = 9;
             this.radioButton4.TabStop = true;
             this.radioButton4.Text = "Car";
@@ -199,7 +201,7 @@ namespace city_planner
             this.label3.Location = new System.Drawing.Point(10, 420);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(104, 17);
+            this.label3.Size = new System.Drawing.Size(126, 20);
             this.label3.TabIndex = 8;
             this.label3.Text = "Estimated time:";
             // 
@@ -209,7 +211,7 @@ namespace city_planner
             this.textBox3.Margin = new System.Windows.Forms.Padding(4);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(312, 23);
+            this.textBox3.Size = new System.Drawing.Size(312, 26);
             this.textBox3.TabIndex = 7;
             // 
             // label2
@@ -218,7 +220,7 @@ namespace city_planner
             this.label2.Location = new System.Drawing.Point(10, 386);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.Size = new System.Drawing.Size(81, 20);
             this.label2.TabIndex = 6;
             this.label2.Text = "Distance:";
             // 
@@ -228,7 +230,7 @@ namespace city_planner
             this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(312, 23);
+            this.textBox2.Size = new System.Drawing.Size(312, 26);
             this.textBox2.TabIndex = 5;
             // 
             // textBox1
@@ -236,7 +238,7 @@ namespace city_planner
             this.textBox1.Location = new System.Drawing.Point(139, 9);
             this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(312, 23);
+            this.textBox1.Size = new System.Drawing.Size(312, 26);
             this.textBox1.TabIndex = 4;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -246,7 +248,7 @@ namespace city_planner
             this.label1.Location = new System.Drawing.Point(10, 14);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 17);
+            this.label1.Size = new System.Drawing.Size(119, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "Characteristic:";
             // 
@@ -259,10 +261,10 @@ namespace city_planner
             this.tabPage3.Controls.Add(this.textBox4);
             this.tabPage3.Controls.Add(this.radioButton8);
             this.tabPage3.Controls.Add(this.radioButton7);
-            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(459, 450);
+            this.tabPage3.Size = new System.Drawing.Size(459, 446);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Characteristics";
             // 
@@ -299,7 +301,7 @@ namespace city_planner
             // 
             this.textBox4.Location = new System.Drawing.Point(17, 88);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(158, 23);
+            this.textBox4.Size = new System.Drawing.Size(158, 26);
             this.textBox4.TabIndex = 2;
             // 
             // radioButton8
@@ -307,7 +309,7 @@ namespace city_planner
             this.radioButton8.AutoSize = true;
             this.radioButton8.Location = new System.Drawing.Point(8, 43);
             this.radioButton8.Name = "radioButton8";
-            this.radioButton8.Size = new System.Drawing.Size(174, 21);
+            this.radioButton8.Size = new System.Drawing.Size(207, 24);
             this.radioButton8.TabIndex = 1;
             this.radioButton8.TabStop = true;
             this.radioButton8.Text = "Highlight objects with ...";
@@ -319,7 +321,7 @@ namespace city_planner
             this.radioButton7.AutoSize = true;
             this.radioButton7.Location = new System.Drawing.Point(8, 8);
             this.radioButton7.Name = "radioButton7";
-            this.radioButton7.Size = new System.Drawing.Size(136, 21);
+            this.radioButton7.Size = new System.Drawing.Size(161, 24);
             this.radioButton7.TabIndex = 0;
             this.radioButton7.TabStop = true;
             this.radioButton7.Text = "Get object details";
@@ -332,26 +334,36 @@ namespace city_planner
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cityPlan1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
-            this.cityPlan1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.cityPlan1.Characteristic = "";
             this.cityPlan1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.cityPlan1.End = ((long)(-1));
             this.cityPlan1.FirstX = -1;
             this.cityPlan1.FirstY = -1;
             this.cityPlan1.IndexTab = 0;
-            this.cityPlan1.Location = new System.Drawing.Point(40, 80);
+            this.cityPlan1.Location = new System.Drawing.Point(-1, -1);
             this.cityPlan1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cityPlan1.Name = "cityPlan1";
-            this.cityPlan1.Size = new System.Drawing.Size(738, 547);
+            this.cityPlan1.Size = new System.Drawing.Size(777, 682);
             this.cityPlan1.Start = ((long)(-1));
             this.cityPlan1.TabIndex = 2;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(981, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 132);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
             // CityPlanner
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
             this.ClientSize = new System.Drawing.Size(1313, 681);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.cityPlan1);
             this.Controls.Add(this.tabControl1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
@@ -367,6 +379,7 @@ namespace city_planner
             this.tabPage2.PerformLayout();
             this.tabPage3.ResumeLayout(false);
             this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -396,6 +409,7 @@ namespace city_planner
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
